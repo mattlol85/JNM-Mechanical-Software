f669af1673804074adf04686abfa6a2defc7800a
BuildTools <unconfigured@null.spigotmc.org>
2019-11-06 19:00:47 +0000

    Worked on MVC

--- org/doomdns/fitznet/jimmy/Controller.java
+++ org/doomdns/fitznet/jimmy/Controller.java
@@ -1,5 +1,15 @@
 package org.doomdns.fitznet.jimmy;
 
 public class Controller{
+    private GUI view;
+    private DatabaseManager databaseModel;
+
+    public Controller(GUI g, DatabaseManager dbm){
+        view = g;
+        databaseModel = dbm;
+        //initView();
+    }
+    public void initView(){
     
+    }
 }
